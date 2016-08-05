10a59f71a2e30a3692e01a16fbd2d539cd7926f1
Matt Vollrath <matt@endpoint.com>
2016-08-05 12:32:25 -0400

    Tweak GStreamer test script

--- gst_test.sh
+++ gst_test.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-gst-launch-1.0 -v videotestsrc ! vp8enc deadline=33333 ! rtpvp8pay ! udpsink sync=false host=127.0.0.1 port=6000
+WIDTH=640
+HEIGHT=400
+BITRATE=768000
+
+gst-launch-1.0 -v videotestsrc is-live=true ! capsfilter caps=video/x-raw,width=$WIDTH,height=$HEIGHT,framerate=30/1 ! timeoverlay ! vp8enc deadline=33333 target-bitrate=$BITRATE ! rtpvp8pay ! udpsink sync=false host=127.0.0.1 port=6000
