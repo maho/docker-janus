cfd0afedba37d2d5637047f35aae0ba2191b7b90
Łu Ma <maho@pagema.net>
2021-03-21 14:56:46 +0100

    - yay! I'm able to stream real video

--- gst_test/run.sh
+++ gst_test/run.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-WIDTH=1920
-HEIGHT=1080
+WIDTH=320
+HEIGHT=240
 BITRATE=1024000
+SERVER=${JANUS_SERVER:-localhost}
+VIDEO_DEVICE=${VIDEO_DEVICE:-/dev/video0}
 
-gst-launch-1.0 -v videotestsrc is-live=true \
-! capsfilter caps=video/x-raw,width=$WIDTH,height=$HEIGHT,framerate=30/1 \
+gst-launch-1.0 -v v4l2src device=/dev/video0  \
+! capsfilter caps=video/x-raw,width=$WIDTH,height=$HEIGHT \
+! videoconvert \
 ! timeoverlay \
 ! queue \
 ! vp8enc deadline=33333 cpu-used=16 target-bitrate=$BITRATE keyframe-max-dist=90 max-quantizer=24 \
 ! rtpvp8pay \
-! udpsink host=janus port=6000 sync=false qos=true
+! udpsink host=$SERVER port=6000 sync=false qos=true
